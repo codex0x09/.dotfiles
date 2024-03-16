da4f02cdbbd2db9c8265d5504db551a80c40d3c2
codex-0x1 <121833227+codex0x09@users.noreply.github.com>
2024-03-16 17:25:40 +0200

    enable auto install parsers

--- .config/nvim/lua/codex/treesitter.lua
+++ .config/nvim/lua/codex/treesitter.lua
@@ -7,11 +7,11 @@ end
 configs.setup{
   ensure_installed = {"c","cpp","php","html","css","javascript","lua","vim","vimdoc","bash"}, --One of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = true, -- Install languages synchronously (only applied to 'ensure_installed')
-  auto_install = false, -- Recommendation: set to false {it dose:Auto install missing parsers}
+  auto_install = true, -- Recommendation: set to false {it dose:Auto install missing parsers}
   ignore_install = { "" }, --List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
   },
-  filtype = vim.treesitter.language.register("php","html"),
+  --filtype = vim.treesitter.language.register("php","html"),
   indent = { enable = true, disable = { "yaml" } },
 }
