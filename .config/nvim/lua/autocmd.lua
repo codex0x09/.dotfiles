0186a1285633d1ff00b1f72042966f44ee3de2ff
codex-0x1 <121833227+codex0x09@users.noreply.github.com>
2024-04-24 15:00:05 +0200

    update autocmd.lua and options.lua

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -5,39 +5,37 @@
 -- Define autocommands with Lua APIs
 -- See: h:api-autocmd, h:augroup
 
-local augroup = vim.api.nvim_create_augroup   -- Create/get autocommand group
-local autocmd = vim.api.nvim_create_autocmd   -- Create autocommand
+local augroup = vim.api.nvim_create_augroup -- Create/get autocommand group
+local autocmd = vim.api.nvim_create_autocmd -- Create autocommand
 
 -- General settings:
 --------------------
 
 -- Highlight on yank
-augroup('YankHighlight', { clear = true })
-autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '1000' })
-  end
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+	group = "YankHighlight",
+	callback = function()
+		vim.highlight.on_yank({ higroup = "IncSearch", timeout = "1000" })
+	end,
 })
 
 -- Remove whitespace on save
-autocmd('BufWritePre', {
-  pattern = '',
-  command = ":%s/\\s\\+$//e"
+autocmd("BufWritePre", {
+	pattern = "",
+	command = ":%s/\\s\\+$//e",
 })
 
 -- Disable auto comment on Buffer Enter
 
-autocmd('BufEnter', {
-  command = ":set formatoptions-=cro"
+autocmd("BufEnter", {
+	command = ":set formatoptions-=cro",
 })
 
-
 -- Set indentation to 2 spaces
-augroup('setIndent', { clear = true })
-autocmd('Filetype', {
-  group = 'setIndent',
-  pattern = { 'xml', 'html', 'xhtml', 'css', 'scss', 'javascript', 'typescript', 'yaml', 'lua'
-  },
-  command = 'setlocal shiftwidth=2 tabstop=2'
+augroup("setIndent", { clear = true })
+autocmd("Filetype", {
+	group = "setIndent",
+	pattern = { "xml", "html", "xhtml", "css", "scss", "javascript", "php", "typescript", "yaml", "lua" },
+	command = "setlocal shiftwidth=2 tabstop=2",
 })
